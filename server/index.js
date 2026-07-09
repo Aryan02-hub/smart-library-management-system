4626bd4129b772c8a5d2b39f14ff804a9ec2d83c
Aryan02-hub <aryanmishra02082004@gmail.com>
2026-07-09 16:41:15 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-const User = require("./models/user");
+const User = require("./models/User");
 const Book = require("./models/Book");
 const Issue = require("./models/Issue");
 
